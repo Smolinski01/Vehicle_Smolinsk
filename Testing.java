703d60805f1874323b12bd23f590b7d2a55d0eaf
Smolinski01 <59387299+Smolinski01@users.noreply.github.com>
2020-05-17 17:39:58 +0200

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,15 @@
+package testing;
+
+import testing.pojazdy.Pojazdy;
+import testing.pojazdy.Ciezarowe;
+import testing.pojazdy.Motory;
+import testing.pojazdy.Samochody;
+
+public class Testing {
+
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
